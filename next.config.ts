26cf65e96639a59cfc31181e8419b00e92bf6e72
i-X-ce <iceci2804@gmail.com>
2026-06-18 02:26:32 +0900

    🔧 Next.config の変更

--- next.config.ts
+++ next.config.ts
@@ -1,7 +1,19 @@
 import type { NextConfig } from "next";
 
+export const isDev = process.env.NODE_ENV === "development";
+
+const repoName = "modal-animation-research-system-demo";
+
 const nextConfig: NextConfig = {
   /* config options here */
+
+  output: "export",
+
+  basePath: isDev ? "" : `/${repoName}`,
+
+  images: {
+    unoptimized: true,
+  },
 };
 
 export default nextConfig;
