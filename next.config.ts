438a845b530cc2d0e8082ac815a8f43f8ac936d7
i-X-ce <iceci2804@gmail.com>
2026-06-24 21:16:19 +0900

    💚 設定を変えてビルドテスト

--- next.config.ts
+++ next.config.ts
@@ -2,14 +2,14 @@ import type { NextConfig } from "next";
 
 export const isDev = process.env.NODE_ENV === "development";
 
-const repoName = "modal-animation-research-system-demo";
+// const repoName = "modal-animation-research-system-demo";
 
 const nextConfig: NextConfig = {
   /* config options here */
 
   output: "export",
 
-  basePath: isDev ? "" : `/${repoName}`,
+  // basePath: isDev ? "" : `/${repoName}`,
 
   images: {
     unoptimized: true,
