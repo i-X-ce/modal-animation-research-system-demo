c67c27b93d26c8b8faeb7660100d091b79d52845
i-X-ce <iceci2804@gmail.com>
2026-07-07 03:37:09 +0900

    🩹 拡張子が二重になっている問題を修正

--- app/viewtransition-demo2/_stores/systemStore.ts
+++ app/viewtransition-demo2/_stores/systemStore.ts
@@ -222,15 +222,13 @@ export const useSystemStore = create<SystemState>((set, get) => ({
 }));
 
 const fileName = (extension: "json" | "csv") =>
-  `system_log_${
-    new Date()
-      .toLocaleString("ja-JP", {
-        year: "numeric",
-        month: "2-digit",
-        day: "2-digit",
-        hour: "2-digit",
-        minute: "2-digit",
-        second: "2-digit",
-      })
-      .replace(/\D/g, "") + `.${extension}`
-  }.${extension}`;
+  `system_log_${new Date()
+    .toLocaleString("ja-JP", {
+      year: "numeric",
+      month: "2-digit",
+      day: "2-digit",
+      hour: "2-digit",
+      minute: "2-digit",
+      second: "2-digit",
+    })
+    .replace(/\D/g, "")}.${extension}`;
