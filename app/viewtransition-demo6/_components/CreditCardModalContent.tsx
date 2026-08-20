f1b6f268a1ab9147815ad11153ba71cd61a8c776
i-X-ce <iceci2804@gmail.com>
2026-08-20 18:34:38 +0900

    💄 モーダルにタイトルをつける

--- app/viewtransition-demo6/_components/CreditCardModalContent.tsx
+++ app/viewtransition-demo6/_components/CreditCardModalContent.tsx
@@ -4,6 +4,7 @@ import {
   Button,
   Checkbox,
   DialogContent,
+  DialogTitle,
   FormControlLabel,
   Stack,
   Typography,
@@ -45,53 +46,56 @@ const CreditCardModalContent = ({ id }: CreditCardContentModalProps) => {
   const { imageCreditCard, textCreditCard, submitted, checked } = props;
 
   return (
-    <DialogContent className="h-full flex-1 flex">
-      <div className="w-1/2 p-4">
-        <CreditCardImage {...imageCreditCard} />
-      </div>
-
-      <Stack component="form" sx={{ flex: 1 }} onSubmit={handleSubmit}>
-        <Stack spacing={4} sx={{ flex: 1, py: 4 }}>
-          <Typography gutterBottom color="textSecondary">
-            誤りのある箇所をチェックして、送信ボタンを押してください。
-          </Typography>
-          {Object.entries(textCreditCard).map(([key, value]) => {
-            if (key === "id") return null;
-            const checkKey = key as keyof typeof checked;
-
-            return (
-              <Stack key={key} spacing={1}>
-                <Typography variant="h6">
-                  {CREDIT_CARD_KEYS_LABELS[checkKey]}
-                </Typography>
-                <Stack direction={"row"} sx={{ alignItems: "center" }}>
-                  <FormControlLabel
-                    onChange={() => toggleChecked(id, checkKey)}
-                    checked={checked?.[checkKey]}
-                    control={<Checkbox />}
-                    label={value}
-                    disabled={submitted}
-                  />
-                </Stack>
-              </Stack>
-            );
-          })}
-        </Stack>
-        <div className="flex justify-center">
-          <div className="basis-100">
-            <Button
-              variant="contained"
-              fullWidth
-              disabled={submitted}
-              type="submit"
-              loading={isPending}
-            >
-              {isPending ? "送信中..." : submitted ? "送信済み" : "送信"}
-            </Button>
-          </div>
+    <div className="h-full flex flex-col">
+      <DialogTitle>内容チェック</DialogTitle>
+      <DialogContent className="flex-1 flex">
+        <div className="w-1/2 p-4">
+          <CreditCardImage {...imageCreditCard} />
         </div>
-      </Stack>
-    </DialogContent>
+
+        <Stack component="form" sx={{ flex: 1 }} onSubmit={handleSubmit}>
+          <Stack spacing={4} sx={{ flex: 1 }}>
+            <Typography gutterBottom color="textSecondary">
+              誤りのある箇所をチェックして、送信ボタンを押してください。
+            </Typography>
+            {Object.entries(textCreditCard).map(([key, value]) => {
+              if (key === "id") return null;
+              const checkKey = key as keyof typeof checked;
+
+              return (
+                <Stack key={key} spacing={1}>
+                  <Typography variant="h6">
+                    {CREDIT_CARD_KEYS_LABELS[checkKey]}
+                  </Typography>
+                  <Stack direction={"row"} sx={{ alignItems: "center" }}>
+                    <FormControlLabel
+                      onChange={() => toggleChecked(id, checkKey)}
+                      checked={checked?.[checkKey]}
+                      control={<Checkbox />}
+                      label={value}
+                      disabled={submitted}
+                    />
+                  </Stack>
+                </Stack>
+              );
+            })}
+          </Stack>
+          <div className="flex justify-center mt-4">
+            <div className="basis-100">
+              <Button
+                variant="contained"
+                fullWidth
+                disabled={submitted}
+                type="submit"
+                loading={isPending}
+              >
+                {isPending ? "送信中..." : submitted ? "送信済み" : "送信"}
+              </Button>
+            </div>
+          </div>
+        </Stack>
+      </DialogContent>
+    </div>
   );
 };
 
